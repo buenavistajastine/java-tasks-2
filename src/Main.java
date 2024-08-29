b1b68426fc2a20e1fe3f39541570d678b038d7c1
githubrepo-next <132056318+buenavistajastine@users.noreply.github.com>
2024-08-29 15:27:26 +0800

    added hashset

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.HashSet;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
     public static void main(String[] args) {
 
         // use constructor if data columns not large
         Self myself = new Self("Earl", 22);
+        HashSet<Integer> nums = new HashSet<Integer>();
+        nums.add(4);
+        nums.add(6);
+        nums.add(9);
+
         myself.updateCondition(false, true);
 
         System.out.println("My name is " + myself.getName() + ". \nI am " + myself.getAge() + " years old.");
@@ -21,6 +35,17 @@ public class Main {
             System.out.println(myself.getName() + " cannot work.");
         }
 
+        for ( Level myEnums: Level.values()) {
+            System.out.println(myEnums);
+        }
+
+        for(int i = 1; i <= 10; i++) {
+            if (nums.contains(i)) {
+                System.out.println(i + " was found in the set.");
+            } else {
+                System.out.println(i + " was not found in the set.");
+            }
+        }
         DemoTest demo = new DemoTest();
         demo.bark();
         demo.scream();
